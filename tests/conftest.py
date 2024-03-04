1213141c708051f13b3b4550026460c7cef5e36d
Irina.Voron <yarutich.irina@gmail.com>
2024-03-04 12:57:13 +0300

    Updated selectors

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@ from selene import browser
 @pytest.fixture(autouse=True)
 def browser_size():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 900
-    browser.config.window_height = 1200
+    browser.config.window_width = 1896
+    browser.config.window_height = 1080
 
     yield
 
--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -2,33 +2,44 @@ from selene import browser, command, by, have
 import os
 
 
-def test_form_filling():
-    browser.open("/automation-practice-form")
-    browser.element("#firstName").type('Irina')
-    browser.element("#lastName").type("Voron")
-    browser.element('#userEmail').with_(type_by_js=True).type('voron@gmail.com')
-    browser.element('[for="gender-radio-2"]').click()
-    browser.element('#userNumber').type('1234567890')
-    browser.element('#dateOfBirthInput').perform(command.js.scroll_into_view).click()
-    browser.element('.react-datepicker__month-select').click().element(by.text('February')).click()
-    browser.element('.react-datepicker__year-select').click().element(by.text('1970')).click()
-    browser.element(by.text('28')).click()
-    browser.element('#subjectsInput').type('english').press_enter()
-    browser.element('[for="hobbies-checkbox-1"]').perform(command.js.scroll_into_view).click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('picture.jpg'))
-    browser.element('#currentAddress').type('NCR, Delhi')
-    browser.element('#state').click().element('#react-select-3-option-0').click()
-    browser.element('#city').click().element('#react-select-4-option-0').click()
-    browser.element('#submit').click()
-    browser.element('[class~="modal-title"]').should(have.exact_text('Thanks for submitting the form'))
+def test_registration_form():
+    browser.open('/automation-practice-form')
 
-    browser.element('.table').should(have.text('Irina Voron'))
-    browser.element('.table').should(have.text('voron@gmail.com'))
-    browser.element('.table').should(have.text('Female'))
-    browser.element('.table').should(have.text('1234567890'))
-    browser.element('.table').should(have.text('28 February,1970'))
-    browser.element('.table').should(have.text('English'))
-    browser.element('.table').should(have.text('Sports'))
-    browser.element('.table').should(have.text('picture.jpg'))
-    browser.element('.table').should(have.text('NCR, Delhi'))
-    browser.element('.table').should(have.text('NCR Delhi'))
+    # WHEN
+    browser.element('#firstName').type('firstName')
+    browser.element('#lastName').type('lastName')
+    browser.element('#userEmail').type('email@example.com')
+    browser.element('[name=gender][value=Female]+label').click()
+    browser.element('#userNumber').type('1234567890')
+
+    browser.element('#dateOfBirthInput').perform(command.js.scroll_into_view).click()
+    browser.element('.react-datepicker__year-select').click().element(by.text('1985')).click()
+    browser.element('.react-datepicker__month-select').click().element(by.text('April')).click()
+    browser.element(f'.react-datepicker__day--0{18}').click()
+
+    browser.element('#subjectsInput').type('english').press_enter()
+    browser.all('[for^=hobbies-checkbox]').element_by(have.text('Sports')).perform(command.js.scroll_into_view)
+    browser.all('[for^=hobbies-checkbox]').element_by(have.text('Sports')).click()
+    browser.element('#uploadPicture').send_keys(os.path.abspath('ressources/picture.jpg'))
+    browser.element('#currentAddress').type('Some Street, 1')
+    browser.element('#state').click()
+    browser.all('[id^=react-select][id*=option]').element_by(have.exact_text('NCR')).click()
+    browser.element('#city').click()
+    browser.all('[id^=react-select][id*=option]').element_by(have.exact_text('Delhi')).click()
+
+    browser.element('#submit').click()
+
+    # THEN
+    browser.element('.table').all('td').even.should(have.texts(
+        'firstName lastName',
+        'email@example.com',
+        'Female',
+        '1234567890',
+        '18 April,1985',
+        'English',
+        'Sports',
+        'picture.jpg',
+        'Some Street, 1',
+        'NCR Delhi'
+        )
+    )
